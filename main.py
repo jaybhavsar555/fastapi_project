dbfbae707168159ce807be446b7a2267bd1fcb7c
Jay Bhavsar <jaybhavsar629@gmail.com>
2026-03-03 14:54:04 +0530

    Add .gitignore, remove venv

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request
+# from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+posts : list[dict] = [
+    {"id": 1, "author" : "Jay Bhavsar" ,"title": "First Post", "content": "This is the first post."},
+    {"id": 2, "author" : "Jay Bhavsar" ,"title": "Second Post", "content": "This is the second post."},
+    {"id": 3, "author" : "Jay Bhavsar" ,"title": "Third Post", "content": "This is the third post."}
+    
+]
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request : Request):
+    return templates.TemplateResponse(request, "home.html", {"posts":posts, "title":"Home Page"})
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
